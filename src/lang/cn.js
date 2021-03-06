975b8c6e27cde0612e536e8a4b68102bb367260f
1103409364 <1103409364@qq.com>
2021-03-06 17:27:42 +0800

    save

--- src/lang/cn.js
+++ src/lang/cn.js
@@ -3,10 +3,11 @@
 const lang = {
   contact: '联系方式',
   experience: '工作经历',
-  education: '教育经历',
-  skills: '技能专长',
-  projects: '项目',
+  education: '教育背景',
+  skills: '职业技能',
+  otherSkills: '其他技能',
+  projects: '项目经验',
   contributions: '贡献',
-  about: '自我介绍'
+  about: '基本信息'
 };
 export default lang;
--- src/resumes/side-bar-projects.vue
+++ src/resumes/side-bar-projects.vue
@@ -1,95 +1,121 @@
 <template>
   <div id="resume2" class="resume">
-    <div class="top-row">
-      <span class="person-name"> {{ person.name.first }} {{ person.name.last }} </span>
-      <span class="person-position"> {{ person.position }} </span>
-    </div>
-    <div class="left-col">
-      <div class="person-image">
-        <div class="image-centerer">
-          <div class="img"></div>
-        </div>
+    <div class="triangle"></div>
+    <div class="triangle-bottom"></div>
+    <div class="main">
+      <div class="top-row">
+        <span class="person-name"> {{ person.name.first + person.name.last }} </span>
+        <span class="person-position"> {{ person.position }} </span>
+        <span class="sal person-position">{{ person.sal }}</span>
       </div>
-      <div class="contact">
-        <h3>Contact</h3>
-        <div class="contact-row">
-          <a :href="'mailto:' + person.contact.email">{{ person.contact.email }}</a>
-        </div>
-        <div class="contact-row dots">
-          <i class="fa fa-circle" aria-hidden="true"></i>
-          <i class="fa fa-circle" aria-hidden="true"></i>
-          <i class="fa fa-circle" aria-hidden="true"></i>
-        </div>
-        <div class="contact-row">
-          <a href="'tel:'+person.contact.phone">{{ person.contact.phone }}</a>
-        </div>
-        <div class="contact-row dots">
-          <i class="fa fa-circle" aria-hidden="true"></i>
-          <i class="fa fa-circle" aria-hidden="true"></i>
-          <i class="fa fa-circle" aria-hidden="true"></i>
-        </div>
-        <div class="contact-row">
-          {{ person.contact.street }} <br />
-          {{ person.contact.city }}
-        </div>
-        <div class="contact-row dots">
-          <i class="fa fa-circle" aria-hidden="true"></i>
-          <i class="fa fa-circle" aria-hidden="true"></i>
-          <i class="fa fa-circle" aria-hidden="true"></i>
-        </div>
-        <div class="contact-row">
-          <a :href="'https://github.com/' + person.contact.github"
-            >https://github.com/{{ person.contact.github }}</a
-          >
-        </div>
-      </div>
-      <div class="skills-block">
-        <h3>Skills</h3>
-        <div class="skills">
-          <div class="skill" v-for="skill in person.skills">
-            <span class="skill-name">{{ skill.name }}</span>
+      <div class="left-col">
+        <!-- <div class="person-image">
+          <div class="image-centerer">
+            <div class="img"></div>
           </div>
+        </div> -->
+        <div class="about">
+          <h3 class="about">{{ lang.about }}</h3>
+          <div class="birth">{{ person.sex }}</div>
+          <div class="birth">{{ person.birth.year }}</div>
+          <div class="birth">{{ person.nation }}</div>
+          <div class="location">{{ person.birth.location }}</div>
+          <div class="location">{{ person.birth.educational }}</div>
         </div>
-        <span class="skills-other"> {{ person.skillDescription }} </span>
-      </div>
-    </div>
-    <div class="right-col">
-      <div class="experience">
-        <h3>Experience</h3>
-        <div class="experience-block" v-for="experience in person.experience">
-          <div class="row">
-            <span class="company"> {{ experience.company }} -</span>
-            <span class="job-title"> {{ experience.position }} </span>
+        <!-- 联系方式 -->
+        <div class="contact">
+          <h3>{{ lang.contact }}</h3>
+          <div class="contact-row">
+            <a href="'tel:'+person.contact.phone">{{ person.contact.phone }}</a>
           </div>
-          <div class="row">
-            <span class="time-period"> {{ experience.timeperiod }}</span>
+          <div class="contact-row">
+            <a :href="'mailto:' + person.contact.email">{{ person.contact.email }}</a>
           </div>
-          <div class="row">
-            <span class="job-description"> {{ experience.description }} </span>
+          <div class="contact-row">
+            {{ person.contact.street }} <br />
+            {{ person.contact.city }}
           </div>
+          <!-- <div class="contact-row">
+          <a :href="'https://github.com/' + person.contact.github">
+            https://github.com/{{ person.contact.github }}
+          </a>
+        </div> -->
         </div>
-      </div>
-      <div class="projects">
-        <h3>Projects</h3>
-        <div class="project-block" v-for="project in person.projects">
-          <div class="row">
-            <span class="name"> {{ project.name }} </span>
-            <div class="row">
-              <span class="time-period"> {{ project.timeperiod }}</span>
-            </div>
-            <div class="row">
-              <span class="description"> {{ project.description }} </span>
-            </div>
-          </div>
-        </div>
+        <!-- 教育经历 -->
         <div class="education">
-          <h3>Education</h3>
-          <div class="education-block" v-for="education in person.education">
+          <h3>{{ lang.education }}</h3>
+          <div class="education-block" v-for="(education, i) in person.education" :key="i">
             <div class="row">
-              <span class="degree">{{ education.degree }}</span>
+              <span class="degree">{{ education.timeperiod }}</span>
             </div>
             <div class="row">
               <span class="degree-description">{{ education.description }}</span>
+            </div>
+            <div class="row">
+              <span class="degree">{{ education.degree }}</span>
+            </div>
+          </div>
+        </div>
+        <!-- 职业技能 -->
+        <div class="skills-block">
+          <h3>{{ lang.skills }}</h3>
+          <div class="skills">
+            <div class="skill" v-for="(skill, i) in person.skills" :key="i">
+              <span class="skill-name">{{ skill.name }}</span>
+            </div>
+          </div>
+          <span class="skills-other"> {{ person.skillDescription }} </span>
+        </div>
+        <!-- 职业技能 -->
+        <div class="skills-block">
+          <h3>{{ lang.otherSkills }}</h3>
+          <div class="skills">
+            <div class="skill" v-for="(skill, i) in person.otherSkills" :key="i">
+              <span class="skill-name">{{ skill.name }}</span>
+            </div>
+          </div>
+          <span class="skills-other"> {{ person.skillDescription }} </span>
+        </div>
+      </div>
+
+      <div class="right-col">
+        <div class="experience">
+          <h3>{{ lang.experience }}</h3>
+          <div class="experience-block" v-for="(experience, i) in person.experience" :key="i">
+            <div class="row">
+              <span class="company"> {{ experience.company }} -</span>
+              <span class="job-title"> {{ experience.position }} </span>
+            </div>
+            <div class="row">
+              <span class="time-period"> {{ experience.timeperiod }}</span>
+            </div>
+            <div class="row">
+              <span class="job-description"> {{ experience.description }} </span>
+            </div>
+          </div>
+        </div>
+        <div class="projects">
+          <h3>{{ lang.projects }}</h3>
+          <div class="project-block" v-for="(project, i) in person.projects" :key="i">
+            <div class="row">
+              <span class="name"> {{ project.name }} </span>
+              <span class="platform">{{ project.platform }}</span>
+              <div class="row project-item">
+                <!-- <div class="label">项目时间：</div> -->
+                <div class="time-period color-info">{{ project.timeperiod }}</div>
+              </div>
+              <div class="row project-item">
+                <div class="label">项目简介：</div>
+                <div class="description color-info">{{ project.description }}</div>
+              </div>
+              <div class="row project-item" v-if="project.duty">
+                <div class="label">我的职责：</div>
+                <div class="duty color-info">{{ project.duty }}</div>
+              </div>
+              <div class="row project-item" v-if="project.result">
+                <div class="label">项目成果：</div>
+                <div class="description color-info">{{ project.result }}</div>
+              </div>
             </div>
           </div>
         </div>
@@ -108,9 +134,16 @@ export default Vue.component(name, getVueOptions(name));
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
+@color1: #3e75b7;
+@color2: #006064;
+
 #resume2 {
-  font-family: 'Raleway', sans-serif;
-  padding-bottom: 50px;
+  height: 57.75cm;
+  // font-family: 'Raleway', sans-serif;
+  // font-family: 'Microsoft Yahei light', sans-serif;
+  font-family: 'Open Sans';
+  padding-bottom: 60px;
+  color: #606266;
   a,
   a:focus,
   a:hover,
@@ -119,35 +152,90 @@ export default Vue.component(name, getVueOptions(name));
   }
   h3 {
     text-transform: uppercase;
-    padding-top: 0;
-    margin-top: 0;
+    // padding-top: 0;
+    // margin-top: 0;
     letter-spacing: 5px;
     font-weight: 400;
+    color: @color1;
+    // text-decoration: underline;
+  }
+  .color-info {
+    color: #82848a;
+  }
+  .triangle {
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 280px 0px 200px 380px;
+    border-color: @color1 transparent transparent transparent;
+    position: absolute;
+    // left: -600px;
+    right: 0;
+    top: 0;
+    z-index: 0;
+  }
+  .triangle-bottom {
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 200px 380px 280px 0px;
+    border-color: transparent transparent @color1 transparent;
+    position: absolute;
+    // left: -600px;
+    left: 0;
+    bottom: -30px;
+    z-index: 0;
+  }
+  .main {
+    position: relative;
+    z-index: 1;
   }
   .top-row {
-    width: 100%;
-    padding-top: 60px;
-    padding-bottom: 60px;
+    width: 26%;
+    padding-left: 8%;
+    padding-right: 4%;
+    padding-top: 30px;
+    padding-bottom: 10px;
+    // background-color:@color2;
+    position: relative;
+    z-index: 1;
+    // color: #fff;
     span {
       width: 100%;
       display: block;
-      text-align: center;
+      text-align: right;
       font-weight: normal;
     }
     span.person-name {
       text-transform: uppercase;
       font-size: 40px;
-      letter-spacing: 10px;
+      letter-spacing: 2px;
     }
     span.person-position {
-      letter-spacing: 5px;
+      letter-spacing: 2px;
     }
   }
   .left-col {
     width: 26%;
     float: left;
-    padding-left: 8%;
-    padding-right: 4%;
+    margin-left: 8%;
+    padding-right: 2%;
+    padding-bottom: 30px;
+    text-align: right;
+    position: relative;
+    // top: -148px;
+    // background-color: #dfe0e2;
+    // color: @main-color;
+    // border-top: 1px solid @color1;
+    h3 {
+      margin-top: 20px;
+      margin-bottom: 10px;
+      // color: @accent-color;
+      // color: #fff;
+      // &:nth-of-type(1) {
+      //   margin-top: 0;
+      // }
+    }
     .person-image .image-centerer {
       display: flex;
       justify-content: center;
@@ -162,108 +250,108 @@ export default Vue.component(name, getVueOptions(name));
         width: 100%;
       }
     }
-    .contact h3 {
-      text-align: center;
-      margin-top: 20px;
-    }
     .contact .contact-row {
-      text-align: center;
       letter-spacing: 2px;
-      margin-bottom: 3px;
+      // margin-bottom: 3px;
       a {
-        color: black;
+        color: #606266;
       }
     }
-    .contact .contact-row:first-of-type {
-      margin-top: 50px;
-    }
-    .contact .contact-row:last-of-type {
-      margin-bottom: 50px;
-    }
-    .contact .contact-row.dots {
-      margin-top: 20px;
-      margin-bottom: 15px;
-      font-size: 10px;
-      color: rgba(153, 153, 153, 0.6);
+    // .contact .contact-row:first-of-type {
+    //   margin-top: 10px;
+    // }
+    // .contact .contact-row:last-of-type {
+    //   margin-bottom: 20px;
+    // }
+    // .contact .contact-row.dots {
+    //   margin-top: 20px;
+    //   margin-bottom: 15px;
+    //   font-size: 10px;
+    //   color: rgba(153, 153, 153, 0.6);
+    // }
+    .skills-block {
+      span.skill-name {
+        letter-spacing: 2px;
+      }
     }
   }
   .right-col {
-    width: 50%;
+    width: 53.85%;
     float: right;
-    padding-left: 4%;
+    padding-left: 2%;
     padding-right: 8%;
+    // border-top: 1px solid @color1;
+    border-left: 1px solid @color1;
+    padding-bottom: 30px;
+    h3 {
+      // color: @main-color;
+      margin-top: 20px;
+      margin-bottom: 10px;
+      // &:nth-of-type(1) {
+      //   margin-top: 0;
+      // }
+    }
     .experience-block {
       margin-bottom: 10px;
+      line-height: 1.5;
       .row:first-child {
         margin-bottom: 3px;
       }
       .row .company {
         text-transform: uppercase;
-        font-size: 12px;
+        font-weight: bold;
       }
       .row .job-title {
-        font-size: 12px;
+        color: #82848a;
+      }
+      .row .time-period {
+        color: #82848a;
+      }
+      .row .job-description {
+        color: #82848a;
       }
     }
     .projects {
-      margin-top: 50px;
+      // margin-top: 50px;
       .project-block {
         margin-bottom: 10px;
+        line-height: 1.5;
         .degree {
-          font-size: 12px;
+          font-size: 14px;
           text-transform: uppercase;
           margin-bottom: 3px;
         }
-      }
-    }
-    .education {
-      margin-top: 50px;
-      .education-block {
-        margin-bottom: 10px;
-        .degree {
-          font-size: 12px;
-          text-transform: uppercase;
-          margin-bottom: 3px;
+        .name {
+          font-weight: bold;
         }
-      }
-    }
-    .skills-block {
-      text-align: center;
-      margin-top: 50px;
-      position: relative;
-      .skills {
-        text-align: center;
-        margin-bottom: 10px;
-        margin-bottom: 20px;
-        position: relative;
-        margin-left: auto;
-        margin-right: auto;
-        display: inline-block;
-        .skill {
-          text-align: center;
-          width: 80px;
-          height: 80px;
-          border-radius: 50%;
-          position: relative;
-          border: #333333 1px solid;
-          margin: 3px;
-          float: left;
-          font-size: 13px;
-          .skill-name {
-            text-align: center;
-            position: absolute;
-            top: 50%;
-            transform: translateY(-50%);
-            width: 100%;
+        .project-item {
+          display: flex;
+          .label {
+            min-width: 80px;
           }
         }
-        .skills-other {
-          display: inline-block;
-          width: 100%;
-          margin-top: 20px;
+        .platform {
+          padding: 2px 6px 4px 6px;
+          margin-left: 10px;
+          background-color: #ecf5ff; //@color1;
+          color: @color1;
+          border-radius: 50px;
+          font-size: 12px;
+          line-height: 20px;
         }
       }
     }
+    // .education {
+    //   margin-top: 50px;
+    //   .education-block {
+    //     margin-bottom: 10px;
+    //     .degree {
+    //       font-size: 12px;
+    //       text-transform: uppercase;
+    //       margin-bottom: 3px;
+    //     }
+    //   }
+    // }
   }
 }
 </style>
